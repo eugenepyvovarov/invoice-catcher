f12d52f7dc7c9189fe3c19225ac7b9a332589f11
Vasiliy Voloshyn <vasiliy.voloshyn@lifeisgoodlabs.com>
2021-11-16 17:53:26 +0200

    removed search field

--- html/resources/js/app.js
+++ html/resources/js/app.js
@@ -13,27 +13,8 @@ $(document).ready(function() {
 
     $('.mask-date-time').mask('0000/00/00 00:00');
 
-
     $('.select2').select2({
         searchInputPlaceholder: 'Search'
     });
-
-    $('.select2-ajax-stations').select2({
-        minimumInputLength: 2,
-        theme: "bootstrap",
-        ajax: {
-            url: '/ajax/stations',
-            dataType: 'json',
-            type: "GET",
-            quietMillis: 50,
-        }
-    });
-
-
-
-    $('.select2-no-search').select2({
-        minimumResultsForSearch: Infinity
-    });
-
 
 });
